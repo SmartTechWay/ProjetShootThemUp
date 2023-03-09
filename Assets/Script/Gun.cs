5455285efbe5d363a4c7b5c1e443c3fad2d80b17
FlorianVst <45539755+FlorianVst@users.noreply.github.com>
2023-03-09 19:16:45 +0100

    Second push
    - Ajout des ennemies.
    - Ajout du mouvement des ennemies.
    - Ajout 2 -ème ennemies qui tire (ne fonctionne pas).

--- Assets/Script/Gun.cs
+++ Assets/Script/Gun.cs
@@ -7,10 +7,34 @@ public class Gun : MonoBehaviour
 
     public Bullet bullet;
 
+    public bool autoshoot = false;
+    public float shootIntervalSecond = 0.5f;
+    public float shootDelaySeconds = 0.0f;
+    float shootTimer = 0f;
+    float delayTimer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (autoshoot)
+        {
+            if (delayTimer >= shootDelaySeconds)
+            {
+                if (shootTimer >= shootIntervalSecond)
+                {
+                    Shoot();
+                    shootTimer = 0;
+                }
+                else
+                {
+                    shootTimer += Time.deltaTime;
+                }
+            }
+            else
+            {
+                delayTimer += Time.deltaTime;
+            }
+        }
     }
 
     // Update is called once per frame
--- Assets/Script/MoveRightLeft.cs
+++ Assets/Script/MoveRightLeft.cs
@@ -2,33 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class MoveRightLeft : MonoBehaviour
 {
-
-    public Vector2 direction = new Vector2(1, 0);
-    public float speed = 2;
-
-    public Vector2 velocity;
-
-    public bool isEnemy = false;
+    public float moveSpeed = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 3);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        velocity = direction * speed;
+        
     }
 
     private void FixedUpdate()
     {
         Vector2 pos = transform.position;
 
-        pos += velocity * Time.fixedDeltaTime;
+        pos.x -= moveSpeed * Time.fixedDeltaTime;
+
+        if (pos.x < -2)
+        {
+            Destroy(gameObject);
+        }
 
         transform.position = pos;
     }
